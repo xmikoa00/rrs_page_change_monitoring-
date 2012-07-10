9c31f0f7cec42fc44f2673b1c69ef3c50dfed078
Karel Benes <xbenes20@stud.fit.atakdale>
2012-07-10 10:54:36 +0200

    Commented debug prints a and set timeout on established connection to 10 sec.
    Debug prints are commented by the "#?" digraph, so they can be told
    from regular comments.

--- resolver.py
+++ resolver.py
@@ -62,32 +62,32 @@ class Resolver(object):
 
     def _make_decision(self, url):
         self.db_metainfo = self._get_metainfo_from_db(url)
-        conn_proxy = _http._HTTPConnectionProxy(url)
+        conn_proxy = _http._HTTPConnectionProxy(url,timeout = 10)
         self.web_metainfo = conn_proxy.send_request("HEAD",url)
 
         store_decision = (0,"Store both header and content")
 
-        print self.db_metainfo
-        print self.web_metainfo
+#?        print self.db_metainfo
+#?        print self.web_metainfo
 
         if self.web_metainfo == None:
             store_decision = (3, "Timeouted")
-            return "Timeouted"
+            return store_decision
 
         try:
             if self.db_metainfo['etag'] == self.web_metainfo[1]['etag']:
                 store_decision = (1, "Store only header (based on etags equality)")
-                return "Nothing changed (etags equal)"
         except KeyError:
             pass
 
         try:
             if self.web_metainfo[1]['content-md5'] == self.db_metainfo['content']['md5']:
                 store_decision = (1, "Store only header (based on recieved content-md5 equality)")
-                return "Nothing changed (md5 equal)"
         except KeyError:
             pass
 
+        if store_decision[0] != 0:
+            return store_decision
 
         # etag and content-md5 are the only authoritave evidents of 'it has not changed'
         # therefore, now is the time to download the content
@@ -96,24 +96,23 @@ class Resolver(object):
         if self.web_full_info == None:
             return "Pruuser, HEAD prosel, GET uz ne"
 
-        print "header: " + self.web_full_info[1]['content-length'] + ", len(): " + str(len(self.web_full_info[2]))
-        print self.web_full_info
+#?        print "header: " + self.web_full_info[1]['content-length'] + ", len(): " + str(len(self.web_full_info[2]))
+#?        print self.web_full_info
 
         mdfiver = hashlib.md5()
         mdfiver.update(self.web_full_info[2])
         self.md5 = mdfiver.hexdigest()
-        print "md5: " + self.md5
+#?        print "md5: " + self.md5
 
         shaoner = hashlib.sha1()
         shaoner.update(self.web_full_info[2])
         self.sha1 = shaoner.hexdigest()
-        print "sha1: " + self.sha1
+#?        print "sha1: " + self.sha1
 
         if self.md5 == self.db_metainfo['content']['md5'] and self.sha1 == self.db_metainfo['content']['sha1']:
             store_decision(1, "Store only header (based on computed md5 and sha1 equality)")
-            return "Nothing changed (md5 and sha1 equal)"
 
-        return "I think they are different, therefore I will store new version"
+        return store_decision
 
 
     def _store_into_db(self, store_decision):
@@ -154,7 +153,7 @@ class Resolver(object):
             'md5': '233fde7ca8a474f4cc7a198ba87822ff',
             'sha1': 'b2e4bce03a0578da5fd9e83b28acac819f365bda',
             'content_type': '',
-            'length': 1234,
+            'length': 1347,
             'urls' : ['http://www.aquafortis.cz/trenink.html']
         }
 
@@ -162,7 +161,7 @@ class Resolver(object):
           'timestamp': 1341161610.287,
           'response_code': 200,
           'last_modified': 'cosi',
-          'etag': '"928169-529-4bf6382fb08c0"',
+          'etag': '"928169-543-4c46b09cfca00"',
           'uid': "rrs_university",
           'url': "http://www.cosi.cz",
           'content': mockup_content  # object_id
